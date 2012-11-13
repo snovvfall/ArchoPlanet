182dec710bc89f09285c271ac1870f8840b309c9
Anton Kustov <snowfall.irk@gmail.com>
2012-11-13 18:08:48 +0900

    Finally scrypt started working.

--- objects_properties.py
+++ objects_properties.py
@@ -1,15 +1,18 @@
 import re, os, bpy, xlrd3
 
-def objectsproperties(sheet_path):
-    wb = xlrd3.open_workbook(sheet_path + "\\" + "irkutsk.xls")
-    "%s\\irkutsk.xls" % sheet_path
+def sheet(sheet_path):
+    wb = xlrd3.open_workbook(sheet_path + "\\" + "Irkutsk.xls")
+    "%s\\Irkutsk.xls" % sheet_path
     wb.sheet_names()
     sh = wb.sheet_by_index(0)
-    cellRow = 1
-    for cellRow from 1 to 99999:
-        bpy.context.scene.frame_set(100)
-        ob = bpy.data.objects[sh.cell(rowx=cellRow,colx=0)]
+    for rownumber in range(1,99999):
+        ob = bpy.data.objects[sh.cell(rownumber,0).value]
         bpy.context.scene.objects.active = ob
-        ob.modifiers[0].thickness = sh.cell(rowx=cellRow,colx=1)
+        bpy.context.scene.frame_set(100)
+        ob.modifiers[0].thickness = sh.cell(rownumber,1).value
         ob.modifiers[0].keyframe_insert(data_path="thickness")
-        cellRow = cellRow + 1
+        bpy.context.scene.frame_set(200)
+        ob.modifiers[0].thickness = sh.cell(rownumber,2).value
+        ob.modifiers[0].keyframe_insert(data_path="thickness")
+        
+sheet('D:\\Dropbox\\2Current\\ArchoPlanet')
